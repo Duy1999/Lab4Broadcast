3509f22c264641d5fbc8805e7fdd1e2c96ce8a0d
duy <duy@1999.com>
2019-09-03 16:39:49 +1000

    Initial commit

---
+++ app/src/main/java/com/example/lab4broadcast/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.lab4broadcast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button sendButton = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sendButton = (Button) findViewById(R.id.sendBtn);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendBroadcast();
+            }
+        });
+    }
+    private void sendBroadcast() {
+        Intent intent = new Intent();
+        intent.setAction("com.example.scottssurface.myapplication");
+        intent.putExtra("Life_form", "_DROID_");
+        sendBroadcast(intent);
+    }
+}
+
