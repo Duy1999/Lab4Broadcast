a784ff993ed0cf4954de29954e6e6c26b5ed2d55
duy <duy@1999.com>
2019-09-03 16:50:34 +1000

    change this?

--- app/src/main/java/com/example/lab4broadcast/MainActivity.java
+++ app/src/main/java/com/example/lab4broadcast/MainActivity.java
@@ -6,9 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button sendButton = null;
+    TextView text = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,5 +30,6 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra("Life_form", "_DROID_");
         sendBroadcast(intent);
     }
+
 }
 
